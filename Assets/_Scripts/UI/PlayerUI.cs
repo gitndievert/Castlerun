538ec33950e9838995a9e6398eca8d4becaa7f59
Wacky Pete <admin@wackypotato.com>
2019-04-15 23:22:39 -0500

    current stuff

--- Assets/_Scripts/UI/PlayerUI.cs
+++ Assets/_Scripts/UI/PlayerUI.cs
@@ -7,8 +7,8 @@ public class PlayerUI
     #region Main Player Stats
     public TextMeshProUGUI PlayerName;
     public TextMeshProUGUI HealthText;
-    public SimpleHealthBar HealthBar;
-    public SimpleHealthBar StaminaBar;
+    //public SimpleHealthBar HealthBar;
+    //public SimpleHealthBar StaminaBar;
 
     public TextMeshProUGUI CastleLevel;
     #endregion
