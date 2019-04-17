e2c576491f763617abe917b660bcf42b4ebbc6f1
Wacky Pete <admin@wackypotato.com>
2019-04-16 23:53:33 -0500

    Camera shake

--- Assets/_Scripts/Helpers/GamePlayHelpers.cs
+++ Assets/_Scripts/Helpers/GamePlayHelpers.cs
@@ -10,4 +10,23 @@ public static class GamePlayHelpers
         int pick = Random.Range(min, max);
         return pick < amount ? pick : amount;
     }
+       
+    public static IEnumerator ShakeCamera(Camera camera, float duration, float intensity)
+    {
+        Vector2 origPos = camera.transform.position;
+        for (float t = 0.0f; t < duration; t += Time.deltaTime * intensity)
+        {
+            // Create a temporary vector2 with the camera's original position modified by a random distance from the origin.
+            Vector2 tempVec = origPos + Random.insideUnitCircle;
+
+            // Apply the temporary vector.
+            camera.transform.position = tempVec;
+
+            // Yield until next frame.
+            yield return null;
+        }
+
+        // Return back to the original position.
+        camera.transform.position = origPos;
+    }
 }
