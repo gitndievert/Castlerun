bc5d25fe6bca0a71e038315216c861fa0d6e2ce1
Wacky Pete <admin@wackypotato.com>
2019-11-12 11:48:27 -0600

    Fixed the camera rotation dampening (set to 0) Added a cam shake test Trying to remove all the jarring parts to the game

---
+++ Assets/_Scripts/Game/Camera/ShakeTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeTest : MonoBehaviour
+{
+    public float TestDuration = 1f;
+    public float TestIntensity = 1f;       
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log($"Testing Shake on Duration({TestDuration}) and Intensity({TestIntensity})");
+            CamShake.Instance.Shake(TestIntensity, TestDuration);
+        }        
+    }
+}
